01189fcf707bd7aa51c95e277dff53794f56795e
Lorena Barragan <lorenabarraganmacias1@gmail.com>
2025-03-15 15:00:06 -0500

    sectcción 2

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,8 +1,15 @@
+import { LayoutPagesComponent } from './pages/layout-pages/layout-pages.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path:'',
+        component:LayoutPagesComponent
+    }
+
+];
 @NgModule({
     imports: [ RouterModule.forChild( routes ) ],
     exports: [ RouterModule],
