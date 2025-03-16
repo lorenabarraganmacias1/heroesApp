4630ac34adefc52b50043e40e3f25146542b57f5
Lorena Barragan <lorenabarraganmacias1@gmail.com>
2025-03-16 10:10:53 -0500

    terminación de rutas hijas y padres

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,12 +1,19 @@
 import { LayoutPagesComponent } from './pages/layout-pages/layout-pages.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
 
 const routes: Routes = [
     {
         path:'',
-        component:LayoutPagesComponent
+        component:LayoutPagesComponent,
+          children:[
+              {path:'login', component:LoginPageComponent},
+              {path:'new-account', component:RegisterPageComponent},
+              {path:'**', redirectTo:'login'},
+            ]
     }
 
 ];
