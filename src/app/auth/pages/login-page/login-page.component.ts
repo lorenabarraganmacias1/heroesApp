d4451d79058c10d4af26820f33348e055e64a843
Lorena Barragan <lorenabarraganmacias1@gmail.com>
2025-03-20 15:28:06 -0500

    definiendo los guard

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { User } from '../../interfaces/user.interfaces';
 
 @Component({
   selector: 'app-login-page',
@@ -6,5 +9,22 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class LoginPageComponent {
+
+
+  constructor (
+    private authService:AuthService,
+    private router: Router
+  ){}
+
+  onLogin():void{
+
+    this.authService.login('fernandoo@gmail.com', '123456')
+    .subscribe(user => {
+
+      this.router.navigate(['/'])
+
+    })
+
+  }
 
 }
